bdafb07f4eef7b21d33a67873986243faa60632c
ashokma <sojiro@ashokma.com>
2020-03-21 22:39:50 +0100

    Netlify deployment with serverless-http

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var app = require('express')();
+const express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const serverless = require('serverless-http');
+const router = express.Router();
 
-app.get('/', function (req, res) {
+router.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
@@ -12,6 +15,13 @@ io.on('connection', function (socket) {
     });
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
-}); 
+// app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+// http.listen(3000, function () {
+//     console.log('listening on *:3000');
+// });
+
+module.exports = app;
+module.exports.handler = serverless(app);
